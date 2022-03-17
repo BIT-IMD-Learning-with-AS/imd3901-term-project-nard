f6d74e7a0f839b54795e1f9c1e5d51dbd2b60239
redchamelon9 <48802350+redchamelon9@users.noreply.github.com>
2022-03-17 13:46:43 -0400

    Adding function for captions
    - Added function (not working yet)

--- public/objInteraction.js
+++ public/objInteraction.js
@@ -36,3 +36,36 @@ AFRAME.registerComponent('blessing', {
             
     
 });
+
+AFRAME.registerComponent('object-info', {
+    init: function () {
+        let el = this.el;
+        let caption = document.createElement('a-text');
+        let scene = document.querySelector('a-scene');
+
+        currentPosition = el.getAttribute('position');
+        
+        this.caption = function(e){
+            caption.setAttribute('position', currentPosition)
+            caption.setAttribute('material','color: #F5F4BC');
+            caption.setAttribute('sound', 'src: /assets/sound/bell.wav; volume: ')
+            caption.setAttribute('animation', 'property: opacity, to: 0; dur: 6000');
+            
+            scene.appendChild(caption);    
+            
+        }
+        
+        this.bless = function(e){
+            glow.setAttribute('animation', 'property: scale; to: 10 10 10; dur: 4000');
+            glow.components.sound.playSound();
+        }
+
+        this.el.addEventListener('click', this.bless);
+    },
+    remove: function () {
+        this.el.removeEventListener('click', this.bless)
+
+    }
+            
+    
+});
