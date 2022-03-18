8431885951d00a5b8c8e590d36e42f1b56136ff5
redchamelon9 <48802350+redchamelon9@users.noreply.github.com>
2022-03-17 23:28:26 -0400

    Added object-info component
     - Object-info component will display the value attribute when the object is clicked
    - Added some more tree assets, very low poly

--- public/objInteraction.js
+++ public/objInteraction.js
@@ -12,8 +12,8 @@ AFRAME.registerComponent('blessing', {
         glow.setAttribute('material','blending: additive');
         glow.setAttribute('material','shader: flat');
         glow.setAttribute('material','src: /assets/glow.png');            
-        glow.setAttribute('scale', '2 2 2');
-        glow.setAttribute('sound', 'src: /assets/sound/bell.wav; volume: ')
+        glow.setAttribute('scale', '0.1 0.1 0.1');
+        glow.setAttribute('sound', 'src: /assets/sound/bell.wav; volume: 0.8')
         
         
         //glow.setAttribute('animation', {property: 'scale', to: {x: 500, y: 500, z: 500}});
@@ -23,15 +23,18 @@ AFRAME.registerComponent('blessing', {
         scene.appendChild(glow);
 
         this.bless = function(e){
-            glow.setAttribute('animation', 'property: scale; to: 10 10 10; dur: 4000');
+            glow.setAttribute('animation', 'property: scale; to: 200 200 200; dur: 10000');
             glow.components.sound.playSound();
+            
         }
 
+        
         this.el.addEventListener('click', this.bless);
+        
     },
     remove: function () {
         this.el.removeEventListener('click', this.bless)
-
+        sceme.removeChild(glow);
     }
             
     
@@ -43,27 +46,28 @@ AFRAME.registerComponent('object-info', {
         let caption = document.createElement('a-text');
         let scene = document.querySelector('a-scene');
 
-        currentPosition = el.getAttribute('position');
-        
-        this.caption = function(e){
-            caption.setAttribute('position', currentPosition)
-            caption.setAttribute('material','color: #F5F4BC');
-            caption.setAttribute('sound', 'src: /assets/sound/bell.wav; volume: ')
-            caption.setAttribute('animation', 'property: opacity, to: 0; dur: 6000');
+        let text = el.getAttribute('value');
+
+        let currentPositionX = el.object3D.position.x - 0.4;
+        let currentPositionY = el.object3D.position.y + 1.5;
+        let currentPositionZ = el.object3D.position.z + 0.3;
+        console.log(currentPositionZ)
+
+        this.description = function(e){
+            caption.setAttribute('position', {x: currentPositionX, y: currentPositionY, z: currentPositionZ});
+            caption.setAttribute('rotation', '-30 0 0');
+            caption.setAttribute('material','color: #FFF');
+            caption.setAttribute('sound', 'src: /assets/sound/bell.wav')
+            //caption.setAttribute('animation', 'property: opacity, to: 0; dur: 6000');
+            caption.setAttribute('value', text);
             
             scene.appendChild(caption);    
             
         }
-        
-        this.bless = function(e){
-            glow.setAttribute('animation', 'property: scale; to: 10 10 10; dur: 4000');
-            glow.components.sound.playSound();
-        }
-
-        this.el.addEventListener('click', this.bless);
+        this.el.addEventListener('click', this.description);
     },
     remove: function () {
-        this.el.removeEventListener('click', this.bless)
+        this.el.removeEventListener('click', this.description)
 
     }
             
